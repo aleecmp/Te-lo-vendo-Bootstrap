a99aa20a4de505f2ecd269a291ebfd26b47bbcbb
aleecmp <ale.cmpb@gmail.com>
2023-03-31 12:48:19 -0300

    feat: add stadistics page

---
+++ js/statistics.js
@@ -0,0 +1,12 @@
+$.fn.dataTable.Api.register('column().data().sum()', function () {
+    return this.reduce(function (a, b) {
+        var x = parseFloat(a) || 0;
+        var y = parseFloat(b) || 0;
+        return x + y;
+    });
+});
+ 
+
+$(document).ready(function () {
+    var table = $('#table-statistics').DataTable();
+});
